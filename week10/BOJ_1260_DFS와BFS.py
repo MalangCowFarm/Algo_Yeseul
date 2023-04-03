2fa6dc7755b65d383cdf383ae2bc56f03e70d61b
yeseul0722 <pdy051257@naver.com>
2023-04-03 17:37:46 +0900

    DFS, BFS

---
+++ week10/BOJ_1260_DFS와BFS.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+def dfs(Start):
+    print(Start, end=' ')
+    visited_dfs[Start] = 1
+    for i in node[Start]:
+        if not visited_dfs[i]:
+            dfs(i)
+
+
+def bfs(S):
+    queue = deque()
+    queue.append(S)
+    visited_bfs[S] = 1
+    while queue:
+        S = queue.popleft()
+        print(S, end=' ')
+        for i in node[S]:
+            if not visited_bfs[i]:
+                queue.append(i)
+                visited_bfs[i] = 1
+
+
+N, M, V = map(int, input().split())
+
+node = [[] for _ in range(N + 1)]
+visited_dfs = [0 for _ in range(N + 1)]
+visited_bfs = [0 for _ in range(N + 1)]
+for i in range(M):
+    u, v = map(int, input().split())
+    node[u].append(v)
+    node[v].append(u)
+
+for n in node:
+    n.sort()
+
+dfs(V)
+print()
+bfs(V)
+print()
