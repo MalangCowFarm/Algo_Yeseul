ae0e8204946a6b156e084a800cd844bce786e304
yeseul0722 <pdy051257@naver.com>
2023-04-07 17:47:28 +0900

    week10

--- week10/BOJ_15723_n단논법.py
+++ week10/BOJ_15723_n단논법.py
@@ -4,11 +4,25 @@ N = int(input())
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 n = len(alphabet)
-graph = [[INF] * n for _ in range(n)]
+graph = [[INF] * n for __ in range(n)]
 
 for _ in range(N):
-    a, b = map(alphabet.index, input().rstrip().split(" is "))
-    print(a, b)
-    graph[a][b] = 1
-for n in graph:
-    print(n)
+    a, b = input().split(' is ')
+    A = alphabet.index(a)
+    B = alphabet.index(b)
+    graph[A][B] = 1
+
+for k in range(n):
+    for y in range(n):
+        for x in range(n):
+            graph[y][x] = min(graph[y][x], graph[y][k] + graph[k][x])
+
+M = int(input())
+for i in range(M):
+    a, b = input().split(' is ')
+    A = alphabet.index(a)
+    B = alphabet.index(b)
+    if graph[A][B] == INF:
+        print('F')
+    else:
+        print('T')
