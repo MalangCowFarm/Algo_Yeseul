61f3a69cdf5b83350afc46e569a19c192d6734f7
yeseul0722 <pdy051257@naver.com>
2023-04-18 18:00:30 +0900

    week12

--- week12/BOJ_1018_체스판다시칠하기.py
+++ week12/BOJ_1018_체스판다시칠하기.py
@@ -1,3 +1,15 @@
 N, M = map(int, input().split())
 board = [list(input().rstrip())for _ in range(N)]
 
+
+def chess(y, x):
+    for i in range(y, y + 7):
+        for j in range(x, x + 7):
+            pass
+
+
+
+
+for y in range(N - 7):
+    for x in range(M - 7):
+        pass
