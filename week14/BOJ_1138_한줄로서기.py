b3bf1fd96a96ecc652d125996b80faebf0ac7074
yeseul0722 <pdy051257@naver.com>
2023-05-03 01:30:13 +0900

    week14

---
+++ week14/BOJ_1138_한줄로서기.py
@@ -0,0 +1,14 @@
+N = int(input())
+H = list(map(int, input().split()))
+result = [0] * N
+
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if cnt == H[i] and result[j] == 0:
+            result[j] = i + 1
+            break
+        elif result[j] == 0:
+            cnt += 1
+
+print(*result)
